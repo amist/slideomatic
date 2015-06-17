704b83979a2547ae20ef530335c6d8a417ad19ec
batz003 <ashaltie@ASHALTIE-WS.cisco.com>
2015-06-17 17:58:13 +0300

    wiki sentences is now better

--- wiki_sentence_generator.py
+++ wiki_sentence_generator.py
@@ -1,32 +1,53 @@
 import wikipedia
 import random
+from concurrent.futures import ThreadPoolExecutor
+import logging
+log = logging.getLogger('pop')
+
+MAX_WORKERS = 12
 
 def get_wiki_summary(phrase):
-    summ = ""
+    summ = []
+    log.info ("pharse=%s", phrase)
     try:
-        summ = wikipedia.summary(phrase)
+        summ.append(wikipedia.summary(phrase).split('. ')[0])
     except wikipedia.exceptions.PageError:
-        words = phrase.split(" ")
+        
+        words = phrase.split()
+        log.debug("words=%s", words)
         if len(words) < 2:
-            return ""
+            log.info('WAT')
+            return []
         else:
-            for word in phrase.split(" "):
-                answer = get_wiki_summary(word).split(".")[0]
-                if answer:
-                    summ += "." + get_wiki_summary(word).split(".")[0]
+            log.debug('popcorn')
+            ex = ThreadPoolExecutor(max_workers=MAX_WORKERS)
+                #summ += list(ex.map(get_wiki_summary, phrase.split()))
+            log.debug ("checking")
+            summ += list(ex.map(get_wiki_summary, phrase.split(' ')))
+            log.debug("checked")
+            if len(summ) == 0:
+                print("got nothing...")
+                return []
+            log.debug('blah')
+                
+            
     except wikipedia.exceptions.DisambiguationError as e:
         suggestions = format(str(e)).split("\n")[1:-1]
+        log.debug('suggestions=%s', suggestions)
         #summ = get_wiki_summary(suggestions[random.randint(0, len(suggestions))])
-        summ = get_wiki_summary(suggestions[0])
+        summ += get_wiki_summary(suggestions[0])
     finally:
         return summ
     
 def random_wiki_sentence(phrase):
     summ = get_wiki_summary(phrase)
     
-    opts = summ.split(". ")
-    for i in opts:        
-        if i and random.random() < 0.5:
-            return i
+    #print("opts = " + format(str(opts)) + "\n")
+    #for i in opts:        
+    #    if i and random.random() < 0.3:
+    #        return i
+    return random.choice(summ)
 
-    return opts[0]
+if __name__ == '__main__':
+    logging.basicConfig(level=0)
+    print(random_wiki_sentence('not like toads who suck balls'))
