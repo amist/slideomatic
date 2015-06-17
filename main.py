6706e659049bd9c482c0e6ec896534bafa6d7827
yglazner <yglazner@yglaznerlt1.IL.NDS.com>
2015-06-17 21:20:26 +0300

    lalala

--- main.py
+++ main.py
@@ -7,16 +7,27 @@ import core
 from core import BACKENDS
 import logging
 
+def inp_m(s):
+    lines = [inp(s)]
+    line = '*'
+    while line:
+        line = input("more:\n")
+        lines.append(line)
+        
+    return "\n".join(lines[:-1])
+        
+
 def inp(s):
     a = ''
     while not a.strip():
         a = input(s)
         with open('lastrun', 'a') as f:
             f.write(a+"\n")
+       
+        
     return a.strip()
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+def main():
     print("Welcome!\n")
     with open('lastrun' ,'wb'): 
         pass
@@ -28,10 +39,22 @@ if __name__ == '__main__':
         para = inp("Insert text for slide %d(q to quit):"%i)
         if para.strip() in 'qQ':
             break
+        line = '*'
+        while 1:
+            line = input("more:\n")
+            if not line: break
+            para += '\n' + line
+    
         paragraphs.append(para)
     backend = ''
     while backend not in BACKENDS:
         backend = inp("Backend [%s]:\n"% ", ".join(BACKENDS.keys()))
         
     core.generate(title, author, paragraphs, backend)
-    print ("Woo hoo!")
+    print ("Woo hooy!")
+    
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
+    
+    
