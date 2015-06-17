559aebf2e929c85686b6c9c1bea813c7e99e60de
yfodor <yfodor@yfodorlt2.IL.NDS.com>
2015-06-17 14:43:29 +0300

    link and slide for text

--- ppt_backend.py
+++ ppt_backend.py
@@ -9,10 +9,13 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-
+#*************************************************************************
+# GREAT LINK:
+# https://python-pptx.readthedocs.org/en/latest/user/quickstart.html
+#*************************************************************************
 
 from pptx import Presentation
-from pptx.util import Inches
+from pptx.util import Inches, Pt
 import urllib2
 
 def download_title_img(img_url, img_name):
@@ -49,17 +52,54 @@ def add_title_slide(prs, title_dict):
         pic = slide.shapes.add_picture(title_dict["image"], left, top, height=height)
 
 
-def contact_slide(prs, contact_dict):
-    pass
+def add_contact_slide(prs, contact_dict):
+    '''
+    Optional: if we want to have contact slide
+    Yoav suggest to pass it as a regular text if needed
+    '''
+    bullet_slide_layout = prs.slide_layouts[1]
+    slide = prs.slides.add_slide(bullet_slide_layout)
+    shapes = slide.shapes
+
+    title_shape = shapes.title
+    body_shape = shapes.placeholders[1]
+
+    title_shape.text = 'Contact Details'
+
+    tf = body_shape.text_frame
+    tf.text = contact_dict["name"]
+    if contact_dict.has_key("email"):
+        p = tf.add_paragraph()
+        p.text = contact_dict["email"]
+    if contact_dict.has_key("phone"):
+        p = tf.add_paragraph()
+        p.text = contact_dict["phone"]
+
+def add_text_slide(prs, slide_dict):
+    blank_slide_layout = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(blank_slide_layout)
+    #position of the text-box
+    left = top = Inches(1)
+    width = height = Inches(5)
+    txBox = slide.shapes.add_textbox(left, top, width, height)
+    tf = txBox.text_frame
+    tf.text = slide_dict["text"]
+
 
 def make_presentation():
     title_dict = {"title":"Go Go Slide-O-Matic",
                     "author":"Slide-O-Matic Team",
                     }#"image":"title_img.jpg"}
+
+    contact_dict = {"name": "The marvelous Team\nTest",
+                    "phone": "10-9",
+                    "email": "marvelous@marvel.com"}
+    slide1 = {"text": "* yair fodor\n* is one of the best\n* fuzball legs up player ever\n"}
     prs = Presentation()
 
     add_title_slide(prs, title_dict)
-
+    add_text_slide(prs, slide1)
+    #add_contact_slide(prs, contact_dict)
     prs.save('test.pptx')
 
 make_presentation()
