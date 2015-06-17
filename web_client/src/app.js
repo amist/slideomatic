53c395c263e5e1c55602744708d4d53fd79abeff
gittyz <gizinger@cisco.com>
2015-06-17 16:52:19 +0300

    added web_client

---
+++ web_client/src/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module('myApp', [])
+myApp.controller('myController', function($scope) {
+    var slide =  function(num){this.num = num;
+        this.title = '';
+    this.content = ''}
+
+    $scope.slides = [new slide(1)]  ;
+
+    $scope.add_slide = function(){
+
+        $scope.slides.push(new slide($scope.slides.length +1))
+        $scope.$apply()
+    }
+
+
+    $scope.apply = function() {
+        alert("generating your slides ......")
+    }
+
+
+    }   )
