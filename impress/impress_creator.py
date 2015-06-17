f17beafe2fd2910bd61e6d6d9faaa4e981bbbbf7
amist <amistern0@gmail.com>
2015-06-17 16:12:48 +0300

    Random movement between slides without cluttering

--- impress/impress_creator.py
+++ impress/impress_creator.py
@@ -1,10 +1,46 @@
+import random
+from math import sqrt
+
 class ImpressCreator(object):
+
+    def __init__(self):
+        self.params_used = []
+        
+    def validate_random_vars(self, vars):
+        [x, y, scale, rotate] = vars
+        for pu in self.params_used:
+            pu_x = pu.get("x")
+            pu_y = pu.get("y")
+            pu_s = pu.get("scale")
+            if sqrt((pu_x - x)**2 + (pu_y - y)**2) < (1000 * pu_s):
+                return False
+        return True
+        
+    def get_random_vars(self, i):
+        heat = 1
+        while True:
+            x = 1000 * i + random.randint(-500*heat, 500*heat)
+            y = 1000 * i + random.randint(-500*heat, 500*heat)
+            scale = random.randint(1, 5)
+            rotate = random.randint(0, 359)
+            if not self.validate_random_vars([x, y, scale, rotate]):
+                heat += 1
+                continue
+            self.params_used.append({"x": x, "y": y, "scale": scale, "rotate": rotate})
+            return [x, y, scale, rotate]
 
     def create_slide(self, desc, i):
         slide_text = ""
-        opening_div = "<div class='step slide' data-x='%d' data-y='%d'>" % (1000 * i, 1000 * i)
+        [x, y, scale, rotate] = self.get_random_vars(i)
+        
+        opening_div = "<div class='step' data-x='%d' data-y='%d' data-scale='%d' data-rotate='%d'>" % (x, y, scale, rotate)
         slide_text += opening_div
         slide_text += desc.get("text")
+        
+        imgs = desc.get("img_list")
+        for img in imgs:
+            slide_text += ("<img width='100%' src='" + img + "'/>")
+        
         slide_text += "</div>"
         return slide_text
         
@@ -27,11 +63,17 @@ class ImpressCreator(object):
                 i += 1
             
             page = page_template % slides_text
-            print(page)
             self.save_file(page, "slides.html")
+            
+            #print(page)
+            #print(self.params_used)
 
 if __name__ == "__main__":
-    descs = [{"text": "my first slide"}, {"text": "my second slide"}]
+    descs = [
+        {"text": "my first slide", "img_list": ["a.jpg"]},
+        {"text": "my second slide", "img_list": ["b.jpg"]},
+        {"text": "my third slide", "img_list": ["c.jpg"]},
+        {"text": "my fourth slide", "img_list": ["d.jpg"]}]
     
     ic = ImpressCreator()
     ic.create_presentation(descs)
