8122357da7abf3aead3789361d3a23160720fda7
gittyz <gizinger@cisco.com>
2015-06-18 00:36:23 +0300

    trying to post create_slides request

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ Created on Jun 17, 2015
 
 @author: yglazner
 '''
-from bottle import Bottle, run, static_file
+from bottle import Bottle, run, static_file, post
 import os
-
+import core
 app = Bottle()
 
 @app.route('/')
@@ -21,6 +21,21 @@ def callback(path):
 
 def main():
     app.run(host='0.0.0.0', port=8080, reloader=True, debug=1)
+	
+@post('/slides_data')
+def create_slides():
+    print "POST Header : \n %s" % dict(request.headers)
+    data = request.json
+    print "data : %s" % data 
+    if data == None:
+        return json.dumps({'Status':"Failed!"})
+    else:
+	core.generate(data.title, data.author, data.paragraphs, data.backend)
+    return json.dumps({'Status':"Success!"})
+
+
+    
+
     
 
 if __name__ == '__main__':
--- web_client/src/app.js
+++ web_client/src/app.js
@@ -5,24 +5,45 @@ myApp.controller('myController', function($scope) {
 
     $scope.title = ""
 
+    $scope.author = ""
+
+    $scope.backend = ""
+
     $scope.slides = [new slide()]  ;
 
     $scope.add_slide = function(){
 
         $scope.slides.push(new slide())
-        $scope.$apply()
+        //$scope.$apply()
     }
 
     $scope.remove_slide = function(slide_index){
 
         $scope.slides.splice(slide_index, 1);
-        $scope.$apply()
+        //$scope.$apply()
     }
 
     $scope.apply = function() {
-        alert("generating your slides ......")
-        alert('slides are '+ JSON.stringify($scope.slides))
+        var paragraphs = []
+         for (s in $scope.slides) {
+             paragraphs.push(s.content)
+         }
+
+        $.post(
+            "slides_data",
+            JSON.stringify(
+                {"tile": $scope.title, "author": $scope.author,
+                "paragraphs" : paragraphs, "backend": $scope.slides}),
+            function (ret_data, st) {
+                $.each(ret_data, function (index, value) {
+                    alert("index: " + index + " , value: " + value);
+                });
+                alert("Server return status : " + st);
+            },
+            'json'
+        );
     }
+
 
 
     }   )
