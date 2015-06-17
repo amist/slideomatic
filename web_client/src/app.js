367608e99c7ed4cabd7ea18d78a42f9e01b87c23
gittyz <gizinger@cisco.com>
2015-06-17 17:18:31 +0300

    added remove_slide

--- web_client/src/app.js
+++ web_client/src/app.js
@@ -1,17 +1,22 @@
 var myApp = angular.module('myApp', [])
 myApp.controller('myController', function($scope) {
-    var slide =  function(num){this.num = num;
+    var slide =  function(){
         this.title = '';
     this.content = ''}
 
-    $scope.slides = [new slide(1)]  ;
+    $scope.slides = [new slide()]  ;
 
     $scope.add_slide = function(){
 
-        $scope.slides.push(new slide($scope.slides.length +1))
+        $scope.slides.push(new slide())
         $scope.$apply()
     }
 
+    $scope.remove_slide = function(slide_index){
+
+        $scope.slides.splice(slide_index, 1);
+        $scope.$apply()
+    }
 
     $scope.apply = function() {
         alert("generating your slides ......")
